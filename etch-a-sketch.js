12c3a4dc39ebaab61438a66db33d7afffd6709b3
hak <hak@tutao.de>
2024-10-16 12:41:35 +0200

    add sketchArea function

---
+++ etch-a-sketch.js
@@ -0,0 +1,21 @@
+const GRIDSIDE = 600;
+let rows = 16;
+let cols = 16;
+
+const sketchArea = document.querySelector("#sketch-area");
+sketchArea.style.width = `${GRIDSIDE}px`;
+sketchArea.style.height = `${GRIDSIDE}px`;
+
+function createGrideCells() {
+  for (let i = 0; 0 < (rows * cols); i++) {
+    const gridCell = document.createElement("div");
+
+    gridCell.style.width = `${GRIDSIDE / cols - 2}px`;
+    gridCell.style.width = `${GRIDSIDE / rows - 2}px`;
+    gridCell.classList.add("cell");
+
+    sketchArea.appendChild(gridCell);
+  }
+}
+
+createGrideCells();
