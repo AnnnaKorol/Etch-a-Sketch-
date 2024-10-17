460993432acf02b1071cbe596d6e680a7e2c62e7
hak <hak@tutao.de>
2024-10-17 16:16:01 +0200

    add mousedown effect

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -17,25 +17,34 @@ const gridToggle = document.querySelector("#grid-toggle");
 let squaresPerSide = 16; //paragraph that shows the sliderValue
 let gridVisible = false;
 
+let isDrawing = false;
+
 function toggleGrid() {
   gridVisible = !gridVisible;
   gridToggle.style.color = gridVisible ? accentColor : inactiveColor;
 
-  removeGridCells();
-  createGridCells();
+  removeGridSquares();
+  createGridSquares();
 }
 
-function setBackgroundColor() {
+function setBackgroundColor(e) {
   //color of one div after overing by mouse the gridboard
-  this.style.backgroundColor = "black";
+if(e.type === "mousedown") {
+ isDrawing = true;
+ e.target.style.backgroundColor = "black";
+} else if(e.type === "mouseover" && isDrawing) {
+e.target.style.backgroundColor = "black"
+}
+else isDrawing = false;
 }
 
-function createGridCells() {
+function createGridSquares() {
   // the number that was provided by user
   const numberOfSquares = squaresPerSide * squaresPerSide; //this number provided by used * by itself (e.g. 30*30)
 
   for (let i = 0; i < numberOfSquares; i++) {
     const gridCell = document.createElement("div"); //create one div in the main div container
+    let widthOrHeight = 0;
 
     if (gridVisible) {
       widthOrHeight = `${parseInt(gridWidth) / squaresPerSide - 2}px`;
@@ -46,12 +55,19 @@ function createGridCells() {
     }
 
     gridCell.style.width = gridCell.style.height = widthOrHeight; // width and height for the one div in the main div container
-    gridCell.addEventListener("mouseover", setBackgroundColor); //when we over the grid with the mouse, the divs become interactive.
+    gridCell.addEventListener("mousedown", (e) => setBackgroundColor(e)); //when we over the grid with the mouse, the divs become interactive.
+    gridCell.addEventListener("mouseover", (e) => setBackgroundColor(e)); //when we over the grid with the mouse, the divs become interactive.
+    gridCell.addEventListener("mouseup", (e) => setBackgroundColor(e)); //when we over the grid with the mouse, the divs become interactive.
+
+    gridCell.addEventListener("dragstart", (e) => {
+      e.preventDefault()
+    });
+
     sketchArea.appendChild(gridCell); //add this one div to the main container
   }
 }
 
-function removeGridCells() {
+function removeGridSquares() {
   // очистки всех дочерних элементов внутри контейнера, который называется sketchArea.
   while (sketchArea.firstChild) {
     //Эта строка проверяет, есть ли у контейнера sketchArea хотя бы один дочерний элемент. firstChild — это первый дочерний элемент контейнера.
@@ -63,10 +79,10 @@ slider.oninput = function () {
   squaresPerSide = this.value;
   //slider.oninput, ты связываешь слайдер с функцией, которая будет выполняться при каждом изменении положения ползунка. Это позволяет, например, динамически обновлять значения на экране без необходимости нажимать кнопку или отправлять форму.
  sliderValue.textContent =`${this.value} x ${this.value} (Resolution)`; // text that provide the slider size
-  removeGridCells(); //
-  createGridCells();
+ removeGridSquares(); //
+ createGridSquares();
 };
 
 gridToggle.addEventListener("click", toggleGrid);
 
-createGridCells();
+createGridSquares();
